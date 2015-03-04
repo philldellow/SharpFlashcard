d476729d2998392fb0094a6c2a2fa8912ac7aa09
Rohith Yeluri <rohithyeluri@users.noreply.github.com>
2015-03-05 11:00:09 +1300

    New ASP.NET MVC file added

---
+++ SharpFlashCard/SharpFlashCard/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SharpFlashCard.Startup))]
+namespace SharpFlashCard
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
