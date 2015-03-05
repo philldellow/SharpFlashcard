f5bd2bc5746c4b819a0a7fef7e65601618ec6825
philldellow <philldellow@gmail.com>
2015-03-06 11:47:32 +1300

    added jquerry animation loader

---
+++ SharpFlashCard/SharpFlashCard/Scripts/loaderAnimationScript.js
@@ -0,0 +1,16 @@
+﻿$(document).on('ready', function () {
+    $("#element").introLoader({
+        animation: {
+            name: 'lettersLoader',
+            options: {
+                ease: "easeInOutCirc",
+                style: 'light',
+                delayBefore: 500,
+                delayAfter: 0,
+                exitTime: 300,
+                loaderText: 'Page ready!',
+                lettersDelayTime: 0
+            }
+        }
+    });
+});
