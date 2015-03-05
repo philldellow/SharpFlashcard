e8de694d189af2d5c545eaa415a0620019f5669b
Rohith Yeluri <rohithyeluri@users.noreply.github.com>
2015-03-05 17:25:35 +1300

    Worked on GameRules model and got it to return a random card

--- SharpFlashCard/SharpFlashCard/Models/GameRules.cs
+++ SharpFlashCard/SharpFlashCard/Models/GameRules.cs
@@ -7,5 +7,12 @@ namespace SharpFlashCard.Models
 {
     public class GameRules
     {
+        Random rnd = new Random();
+        public FlashCards RandomCardPick()
+        {
+            List<FlashCards> _FlashCard = new List<FlashCards>();
+            int r = rnd.Next(1, 39);
+            return _FlashCard[r];
+        }
     }
 }
