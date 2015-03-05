69d3d9b5da354e6fb6f3db6335e5511718934490
philldellow <philldellow@gmail.com>
2015-03-06 12:10:10 +1300

    changed bundle config to remove bug

--- SharpFlashCard/SharpFlashCard/App_Start/BundleConfig.cs
+++ SharpFlashCard/SharpFlashCard/App_Start/BundleConfig.cs
@@ -26,7 +26,7 @@ namespace SharpFlashCard
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css",
-                      "loaderAnimationScript.js"));
+                      "~/loaderAnimationScript.js"));
         }
     }
 }
